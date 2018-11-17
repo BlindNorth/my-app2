c598991ef04696e38a2d97eab96e239c283c4bfb
BlindNorth <a220312443@gmail.com>
2018-11-17 16:34:34 +0300

    Updates

---
+++ precache-manifest.27de9ec3e1ffe0dffbab0b0784d463d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app2/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fa23ed07148923f5e98e",
+    "url": "/my-app2/static/js/runtime~main.fa23ed07.js"
+  },
+  {
+    "revision": "7efe6f57bca01517669f",
+    "url": "/my-app2/static/js/main.7efe6f57.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/my-app2/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "7efe6f57bca01517669f",
+    "url": "/my-app2/static/css/main.50393493.chunk.css"
+  },
+  {
+    "revision": "3264c75aaa26fdca9963e755aae57585",
+    "url": "/my-app2/index.html"
+  }
+];
